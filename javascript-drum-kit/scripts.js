10591af1a36c82c87737d385a5eea61ef0651845
AbbyNZ <28561684+AbbyNZ@users.noreply.github.com>
2018-09-27 11:07:08 +1000

    Updated javascript

--- javascript-drum-kit/scripts.js
+++ javascript-drum-kit/scripts.js
@@ -1,15 +1,29 @@
+function playSound(e) {
+  const audio = e.keyCode ? 
+        document.querySelector(`audio[data-key="${e.keyCode}"]`) :
+        document.querySelector(`audio[data-key="${this.attributes['data-key'].value}"]`);
+  const key = e.keyCode? 
+        document.querySelector(`.key[data-key="${e.keyCode}"]`) :
+        document.querySelector(`.key[data-key="${this.attributes['data-key'].value}"]`);
+  if (!audio) {
+    return;
+  }
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-  }
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+}
+
+const keys = document.querySelectorAll('.key');
+
+document.addEventListener('keydown', playSound);
+
+keys.forEach(key => {
+  key.addEventListener('transitionend', removeTransition);
+
+key.addEventListener('click', playSound);  
+});
